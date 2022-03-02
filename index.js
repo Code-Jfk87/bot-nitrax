ebf63e9f57a3760e37dfe9c7351c10343f7e28f0
Code-Jfk87 <jules.ducouret@gmail.com>
2022-03-02 21:15:23 +0100

    Ajout code du bot

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const {token} = require("./config.json")
+const bot = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ]
+});
+
+
+//préfix commande
+var prefix = "!"
+
+
+bot.on("ready",() => {
+    console.log("Bot opérationnel !")
+});
+
+bot.on("messageCreate", message => {
+    const links = ["discord.gg/"]
+    if (message.content.includes(links)) {
+    message.delete(message)
+    const LinksEmbedWarning = new Discord.MessageEmbed()
+        .setTitle("Warning")
+        .setColor("EA3131")
+        .setDescription("<@" + message.author.id + "> les invitations sont intedit !")
+    message.channel.send({embeds: [LinksEmbedWarning]})
+
+    }
+});
+
+bot.login(token);
