84ac04eb05721a433f51a3904afc7165a31cd147
Code-Jfk87 <jules.ducouret@gmail.com>
2022-03-03 22:10:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var prefix = "!"
 
 bot.on("ready",() => {
     console.log("Bot opérationnel !")
-    bot.cache.get.channel("949046209073520711").send("Je suis pret à travailler !")
+//    bot.cache.get.channel("949046209073520711").send("Je suis pret à travailler !")
 });
                                                                                                                  
                                                                              
